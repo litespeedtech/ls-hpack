9cabf8954e282e9926fdb5574fc675dfd6fab44e
George Wang <gwang@litespeedtech.com>
2023-11-30 10:58:53 -0500

    [BUGFIX] normalize header name with trailing spaces.

--- lshpack.c
+++ lshpack.c
@@ -23,6 +23,7 @@ SOFTWARE.
 */
 
 #include <assert.h>
+#include <ctype.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -1774,6 +1775,11 @@ lshpack_dec_decode (struct lshpack_dec *dec,
         }
         if (len > UINT16_MAX)
             return LSHPACK_ERR_TOO_LARGE;
+        while(len > 0 && isspace(*(name + len - 1)))
+            --len;
+        if (len == 0)
+            return LSHPACK_ERR_BAD_DATA;
+
 #if LSHPACK_DEC_CALC_HASH
         output->flags |= LSXPACK_NAME_HASH;
         output->name_hash = XXH32(name, (size_t) len, LSHPACK_XXH_SEED);
